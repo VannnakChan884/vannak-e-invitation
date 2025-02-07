95d770e3188bf36a8ce65fe1dc4f144f30b99ebf
Chan Vannak <vannakchan884@gmail.com>
2025-02-07 10:29:04 +0700

    9-8

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,100 +1,220 @@
 <template>
+    <!--Content-->
     <div class="container-fluid">
-        <div class="container">
+        <div class="container w-50">
             <div class="row text-center">
-                    <div class="col-lg-12">
-                        <h1 class="text-center mb-4">សិរីមង្គលអាពាហ៍ពិពាហ៍</h1>
-                        <h4>The Wedding Day</h4>
-                    </div>
+                <div class="col-lg-12">
+                    <h1 class="text-center mb-4">សិរីមង្គលអាពាហ៍ពិពាហ៍</h1>
+                    <h4 >The Wedding Day</h4 >
+                </div>
             </div>
             <div class="row mt-5">
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="row">
-                            <div class="col-3">
-                                <h6>លោក</h6>
-                            </div>
-                            <div class="col-9">
-                                <h4>ម៉ឹក ចាន់វាសនា</h4>
-                            </div>
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-3">
+                            <h6>លោក</h6>
+                        </div >
+                        <div class="col-lg-9 col-md-9 col-sm-9">
+                            <h4 >ម៉ឹកចាន់វាសនា</h4 >
+                        </div >
+                    </div >
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-3">
+                            <h6>លោកស្រី</h6>
+                        </div >
+                        <div class="col-lg-9 col-md-9 col-sm-9">
+                            <h4 >លឹមសុន</h4 >
+                        </div >
+                    </div >
+                </div >
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="row text-end">
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <h6>លោក</h6>
+                        </div >
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <h4>ឃិនណី</h4>
+                        </div >
+                    </div >
+                    <div class="row text-end">
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <h6>លោកស្រី</h6>
                         </div>
-                        <div class="row">
-                            <div class="col-3">
-                                <h6>លោកស្រី</h6>
-                            </div>
-                            <div class="col-9">
-                                <h4>លឹម សុន</h4>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="row">
-                            <div class="col-3">
-                                <h6>លោក</h6>
-                            </div>
-                            <div class="col-9">
-                                <h4>ឃិន ណី</h4>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-3">
-                                <h6>លោកស្រី</h6>
-                            </div>
-                            <div class="col-9">
-                                <h4>ឡុង វណ្ណនាង</h4>
-                            </div>
-                        </div>
-                    </div>
+                        <div class="col-lg-6 col-md-6 col-sm-6">
+                            <h4>ឡុងវណ្ណនាង</h4>
+                        </div >
+                    </div >
+                </div>
             </div>
             <div class="row mt-5">
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                        <p class="text-center mb-2">មានកិត្តិយស</p>
-                        <h3 class="text-center mb-2">សូមគោរពអញ្ចើញ</h3>
-                        <p class="text-break lh-lg text-modify">
-                            សម្តេច ទ្រង់ ឯកឧត្តម លោកជំទាវ ​អ្នកឧកញ៉ា ​ឧកញ៉ា លោក លោក​ស្រី អ្នកនាង កញ្ញា 
-                            អញ្ជើញចូលរួមជាអធិបតី និងជាភ្ញៀវកិតិ្តយស ដើម្បីប្រសិទ្ធិពរជ័យ សិរីមង្គលក្នុងពិធីរៀបអាពាហ៍ពិពាហ៍
-                            <br>កូនប្រុស-ស្រី របស់យើងខ្ញុំ
-                        </p>
-                    </div>
-            </div>
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <p class="text-center mb-2">មានកិត្តិយស</p >
+                    <h3 class="text-center mb-2">សូមគោរពអញ្ចើញ</h3>
+                    <p class="text-break lh-lg text-modify">
+                    សម្តេចទ្រង់ ឯកឧត្តម លោកជំទាវ​ អ្នកឧកញ៉ា ​ឧកញ៉ា លោក លោក​ស្រី អ្នកនាងកញ្ញា
+                    អញ្ជើញចូលរួមជាអធិបតី និងជាភ្ញៀវកិតិ្តយស ដើម្បីប្រសិទ្ធិពរជ័យ សិរីមង្គលក្នុងពិធីរៀបអាពាហ៍ពិពាហ៍
+                    <br>កូនប្រុស-ស្រីរបស់យើងខ្ញុំ
+                    </p  >
+                </div >
+            </div >
             <div class="row mt-3">
-                    <div class="col-lg-6 col-md-6 col-sm-6 box">
-                        <div class="text-center">
-                            <div class="row mb-2 m-0">
-                                <div class="col-12 mb-3">
-                                    <p>កូនប្រុសនាម</p>
-                                </div>
-                                <div class="col-12">
-                                    <h4 class="fw-bold">ចាន់ វណ្ណៈ</h4>
-                                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 box">
+                    <div class="text-center">
+                        <div class="row mb-2 m-0">
+                            <div class="col-12 mb-3">
+                                <p>កូនប្រុសនាម</p>
                             </div>
-                            <div class="row m-0">
-                                <div class="box-img col-lg-12 p-0">
-                                    <img src="/images/groom.jpg" alt="Groom">
-                                </div>
+                            <div class="col-12">
+                                <h4 class="text-warning fw-semibold">ចាន់វណ្ណៈ</h4 >
+                            </div >
+                        </div >
+                        <div class="row m-0">
+                            <div class="box-img col-lg-12 p-0">
+                                <img src="/images/groom.jpg" alt="Groom">
+                            </div >
+                        </div >
+                </div >
+                </div >
+                <div class="col-lg-6 col-md-6 col-sm-6 box">
+                    <div class="text-center">
+                        <div class="row mb-2 m-0">
+                            <div class="col-12 mb-3">
+                                <p>កូនស្រីនាម</p>
+                            </div>
+                            <div class="col-12">
+                                <h4 class="text-warning fw-semibold">ឃិនស្រីណយ</h4>
+                            </div >
+                        </div >
+                        <div class="row m-0">
+                            <div class="box-img col-lg-12 p-0">
+                                <img src="/images/bride.jpg" alt="Bride">
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-sm-6 box">
-                        <div class="text-center">
-                            <div class="row mb-2 m-0">
-                                <div class="col-12 mb-3">
-                                    <p>កូនស្រីនាម</p>
-                                </div>
-                                <div class="col-12">
-                                    <h4 class="fw-bold">ឃិន ស្រីណយ</h4>
-                                </div>
-                            </div>
-                            <div class="row m-0">
-                                <div class="box-img col-lg-12 p-0">
-                                    <img src="/images/bride.jpg" alt="Bride">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-12 text-center">
+                    <h3>និងពិសារភោជនាអាហារដែលនឹងប្រព្រឹត្តទៅ</h3>
+                    <p class="mt-2 lh-lg text-modify">
+                    នៅ ថ្ងៃ<span class="text-warning fs-5 fw-medium">ចន្ទ </span>
+                    ទី<span class="text-warning fs-5 fw-medium">១៥ </span>
+                    ខែ<span class="text-warning fs-5 fw-medium">មីនា </span>
+                    ឆ្នាំ<span class="text-warning fs-5 fw-medium">២០២៥ </span>
+                    ត្រូវនឹង ថ្ងៃ<span class="text-warning fs-5 fw-medium">២រោច </span>
+                    ខែ<span class="text-warning fs-5 fw-medium">ផល្គុន </span>
+                    ឆ្នាំ<span class="text-warning fs-5 fw-medium">រោង ឆស័ក </span>
+                    ពុទ្ធសករាជ<span class="text-warning fs-5 fw-medium"> ២៥៦៨ </span>
+                    វេលាម៉ោង<span class="text-warning fs-5 fw-medium"> ៥:០០នាទីល្ងាច </span>
+                    នៅ<span class="text-warning fs-5 fw-medium"> គេហដ្ឋានខាងស្រី </span>
+                    ស្ថិតនៅ ភូមិ<span class="text-warning fs-5 fw-medium">កសិករ </span>
+                    ឃុំ<span class="text-warning fs-5 fw-medium">សំឡាញ </span>
+                    ស្រុក<span class="text-warning fs-5 fw-medium">អង្គជ័យ </span>
+                    ខេត្ត<span class="text-warning fs-5 fw-medium">កំពត </span>ដោយមេត្រីភាព។ សូមអរគុណ!
+                    <br>(សូមពិនិត្យមើលប្លង់បញ្ជាក់)</p>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-12 text-center mb-2">
+                        <h3>កម្មវិធីសិរីសួស្ដីអាពាហ៍ពិពាហ៍</h3>
+                </div>
+                <div class="col-12 text-center">
+                        <h4 class="mt-4 text-decoration-underline">កម្មវិធីថ្ងៃទី១</h4>
+                        <h5 class="mt-4 text-start">ថ្ងៃសុក្រ ទី១៤ ខែមីនា ឆ្នាំ២០២៥</h5>
+                </div>
+                <div class="col-12">
+                        <table class="table table-hover">
+                            <tr>
+                                <td>ម៉ោង ០២ៈ០០ រសៀល</td>
+                                <td>:</td >
+                                <td>ពិធីសែនក្រុងពាលី</td>
+                            </tr>
+                                <tr>
+                                <td>ម៉ោង ០៣ៈ០០ រសៀល</td>
+                                <td>:</td>
+                                <td>ពិធីសូត្រមន្តចម្រើនព្រះបរិត្ត</td>
+                            </tr>
+                            <tr>
+                                <td>ម៉ោង ០៤ៈ០០ រសៀល</td>
+                                <td>:</td>
+                                <td>ពិធីជំពាលានិងជាវខាន់ស្លា</td>
+                            </tr>
+                            <tr>
+                                <td>ម៉ោង ០៥ៈ០០ ល្ងាច</td>
+                                <td>:</td>
+                                <td>អញ្ជើញភ្ញៀវកិត្តិយសពិសាអាហារពេលល្ងាច</td>
+                            </tr>
+                        </table>
+                </div>
+                <div class="col-12 text-center mb-2">
+                        <h4 class="mt-4 text-decoration-underline">កម្មវិធីថ្ងៃទី២</h4>
+                        <h5 class="mt-4 text-start">ថ្ងៃសៅរ៍ ទី១៥ ខែមីនា ឆ្នាំ២០២៥</h5>
+                </div>
+                <div class="col-12">
+                        <table class="table table-hover">
+                            <tr>
+                                <td>ម៉ោង ០៦ៈ៣០ ព្រឹក</td>
+                                <td>:</td>
+                                <td>ជួបជុំភ្ញៀវកិត្តិយសរៀបពិធីចំហែជំនូន</td>
+                            </tr>
+                            <tr>
+                                <td>ម៉ោង ០៧ៈ០០ ព្រឹក</td>
+                                <td>:</td>
+                                <td>ពិធីហែជំនូន(ហែកំណត់) និងអញ្ជើញភ្ញៀវកិត្តយសពិសាអាហារពេលព្រឹក</td>
+                            </tr>
+                            <tr>
+                                <td>ម៉ោង ០៧ៈ៣០ ព្រឹក</td>
+                                <td>:</td>
+                                <td>ពិធីចៅមហានិយាយជើងការរៀបរាប់ផ្លែឈើ</td>
+                            </tr>
+                            <tr>
+                                <td>ម៉ោង ០៨ៈ៣០ ព្រឹក</td>
+                                <td>:</td>
+                                <td>ពិធីបំពាក់ចិញ្ចៀន</td>
+                            </tr>
+                            <tr>
+                                <td>ម៉ោង ០៩ៈ១៥ ព្រឹក</td>
+                                <td>:</td>
+                                <td>ពិធីកាត់សក់បង្កក់សិរី</td >
+                            </tr>
+                            <tr>
+                                <td>ម៉ោង ១១ៈ០០ ថ្ងៃត្រង់</td>
+                                <td>:</td>
+                                <td>ពិធីបង្វិលពពិលសំពះផ្ទឹមសែនចងដៃ</td>
+                            </tr>
+                            <tr>
+                                <td>ម៉ោង ១២ៈ០០ ថ្ងៃត្រង់</td>
+                                <td>:</td>
+                                <td>អញ្ជើញភ្ញៀវកិត្តិយសពិសាអាហារថ្ងៃត្រង់</td>
+                            </tr>
+                            <tr>
+                                <td>ម៉ោង ០៥ៈ០០ ល្ងាច</td>
+                                <td>:</td>
+                                <td>ទទួលភ្ញៀវកិត្តិយសពិសាភោជនាហារ</td>
+                            </tr>
+                        </table>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-12">
+                    <h3 class="text-center">សេចក្ដីថ្លែងអំណរគុណ និងសូមអភ័យទោស</h3>
+                    <p class="lh-lg mt-2 text-modify">យើងខ្ញុំ សូមថ្លែងអំណរគុណយ៉ាងជ្រាលជ្រៅ ចំពោះការអញ្ចើញចូលរួមជាភ្ញៀវកិត្តិយស
+                    ក្នុងពិធីរៀបអាពាហ៍ពិពាហ៍កូនប្រុស-ស្រីរបស់យើងខ្ញុំ និងសូមខន្តី អភ័យទោស
+                    ដោយពុំបានជួបអញ្ចើញដោយផ្ទាល់ និងការសរសេរឈ្មោះរបស់ភ្ញៀវកិត្តិយសមិនបានត្រឹមត្រូវ ឬពុំបានសរសេរឈ្មោះ។
+                    សូមជូនពរសម្តេចទ្រង់ ឯកឧត្តម លោកជំទាវ​ អ្នកឧកញ៉ា ​ឧកញ៉ា លោក លោក​ស្រី អ្នកនាងកញ្ញា
+                    និងភ្ញៀវកិត្តិយសទាំងអស់មានសុខភាពល្អ និងទទួលបានជោគជ័យគ្រប់ភារកិច្ច។</p>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-12 text-center">
+                    <h3>ទីតាំងកម្មវិធី</h3>
+                    <p class="lh-lg mt-2">ភូមិកសិករ ឃុំសំឡាញ ស្រុកអង្គជ័យ ខេត្តកំពត</p>
+                </div>
             </div>
         </div>
     </div>
-    <footer class="container-fluid text-center">
+    <!--Footer-->
+    <div class="container-fluid text-center">
         <div class="container footer-container">
             <div class="row">
                 <div class="col-md-12 col-12 mb-4">
@@ -102,15 +222,15 @@
                 </div>
             </div>
             <div class="footer-copyright">
-                <span>Chan Vannak</span>
-                <p>096 26 65 240</p>
+                <span>ChanVannak</span>
+                <p>0962665240</p>
             </div>
         </div>
-    </footer>
+    </div >
 </template>
-<style scoped>
+<style>
     .container-fluid{
-        background-color: #000000c7;
+        background-color:#000000a9;
         padding: 50px;
     }
     .text-modify{
@@ -123,28 +243,39 @@
         align-items: center;
     }
     .box-img{
-        width: 300px;
-        height: 390px;
-        border: 2px solid skyblue;
-        box-shadow: 1px 1px 20px 0 skyblue;
-        border-radius: 3%;
+        width: 250px;
+        height: 340px;
+        border: 1px solid#fff;
+        box-shadow: 1px 1px 10px 0#ffffff4b;
+        border-radius: 4%;
         overflow: hidden;
         display: flex;
         justify-content: center;
         align-items: center;
     }
     .box-img img{
-        width: 99%;
-        height: 99%;
+        width: 98%;
+        height: 98%;
         object-fit: cover;
-        border-radius: 2%;
+        border-radius: 3%;
     }
-    .footer-container {
-    padding: 50px 0;
-}
+    .footer-container{
+        padding: 50px 0;
+    }
 
-.container-fluid .footer-container .footer-slogain {
-    /* font-size: 18px; */
-    margin-bottom: 15px;
-}
+    .container-fluid .footer-container .footer-slogain{
+        margin-bottom: 15px;
+    }
+    .table>:not(caption)>*>*{
+        background: none;
+        border-bottom-width: 0;
+        box-shadow: none;
+        color:#fff;
+    }
+    .table tr td:first-child{
+        width: 200px;
+    }
+    .table tr td:nth-child(2){
+        width: 30px;
+    }
 </style>
