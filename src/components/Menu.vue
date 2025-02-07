29599b6f4b887c6269e2bddb3035d2ac7e9c4452
Chan Vannak <vannakchan884@gmail.com>
2025-02-07 10:59:04 +0700

    10-5

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -11,18 +11,18 @@
             <div class="row mt-5">
                 <div class="col-lg-6 col-md-6">
                     <div class="row">
-                        <div class="col-lg-3 col-md-3 col-sm-3">
+                        <div class="col-lg-3 col-md-3">
                             <h6>លោក</h6>
                         </div >
-                        <div class="col-lg-9 col-md-9 col-sm-9">
+                        <div class="col-lg-9 col-md-9">
                             <h4 >ម៉ឹកចាន់វាសនា</h4 >
                         </div >
                     </div >
                     <div class="row">
-                        <div class="col-lg-3 col-md-3 col-sm-3">
+                        <div class="col-lg-3 col-md-3">
                             <h6>លោកស្រី</h6>
                         </div >
-                        <div class="col-lg-9 col-md-9 col-sm-9">
+                        <div class="col-lg-9 col-md-9">
                             <h4 >លឹមសុន</h4 >
                         </div >
                     </div >
