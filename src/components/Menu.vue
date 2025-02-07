735c2beba3990e45185851a492090656f9c93a67
Chan Vannak <vannakchan884@gmail.com>
2025-02-07 10:57:04 +0700

    10-4

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -9,7 +9,7 @@
                 </div>
             </div>
             <div class="row mt-5">
-                <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="col-lg-6 col-md-6">
                     <div class="row">
                         <div class="col-lg-3 col-md-3 col-sm-3">
                             <h6>លោក</h6>
@@ -27,7 +27,7 @@
                         </div >
                     </div >
                 </div >
-                <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="col-lg-6 col-md-6">
                     <div class="row text-end">
                         <div class="col-lg-6 col-md-6 col-sm-6">
                             <h6>លោក</h6>
