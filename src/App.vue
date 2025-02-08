e4b0c8fdbfdad290748e4232d508e3c224b7e796
Chan Vannak <vannakchan884@gmail.com>
2025-02-08 12:26:07 +0700

    redirect to home if refreshes

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { onMounted } from 'vue';
+
+const router = useRouter();
+
+onMounted(() => {
+  if (window.location.pathname !== '/') {
+    router.push('/'); // Redirect to Home if refreshed
+  }
+});
+</script>
 <template>
   <RouterView />
 </template>
